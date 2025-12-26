81f1cae3500725e34dc7ea1b726d67755b3bb171
Irina Ostapchuk <by.ingrem@gmail.com>
2025-12-26 12:36:54 +0300

    locked out user test

--- tests/ui/specs/login.spec.ts
+++ tests/ui/specs/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/login.page';
 
-test.describe('Login', () => {
+test.describe('Login procedure', () => {
   test('Successful login', async ({ page }) => {
     const loginPage = new LoginPage(page);
     
@@ -18,9 +18,19 @@ test.describe('Login', () => {
     await loginPage.open();
     await loginPage.login('standard_user', 'wrong_password');
     
-    // Get an error
+    // Get an error message
     const error = await loginPage.getError();
     expect(error).toContain('Username and password do not match');
+  });
+
+  test('Locked out user', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    
+    await loginPage.open();
+    await loginPage.login('locked_out_user', 'secret_sauce');
+    
+    const error = await loginPage.getError();
+    expect(error).toContain('Sorry, this user has been locked out');
   });
 
 });
